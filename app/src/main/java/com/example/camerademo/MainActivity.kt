6664da0a09a5ac1d77c6bdbc2dc5389036ec048b
Mario-paul <15961362+Mario-paul@users.noreply.github.com>
2022-09-23 21:37:09 -0400

    enable viewBinding, add camera permission, more...
    - add version control rules

--- app/src/main/java/com/example/camerademo/MainActivity.kt
+++ app/src/main/java/com/example/camerademo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.camerademo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.camerademo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA = 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
